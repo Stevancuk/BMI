45da7a8a62d8368d116882f212b8532eb98ffafb
Stevan Cuk <stevancuk@yahoo.com>
2019-06-21 20:29:26 +0200

    Adding range inputs

--- js/jsVer1.js
+++ js/jsVer1.js
@@ -1,13 +1,21 @@
 "use strict";
 
+// #########################
+// ### Main Calculations ###
+// #########################
+
 let resultBMI;
 //calculate metric
 function calculateMetricBMI(){
     //Check if all inputs are filled
     if( $('#input_kg').val() && $('#input_cm').val() ) {
-        resultBMI = parseFloat($('#input_kg').val()) / Math.pow( parseFloat($('#input_cm').val()) / 100 ,2);
+        resultBMI = parseFloat($('#input_kg').val()) / Math.pow( parseFloat($('#input_cm').val()) / 100, 2);
         resultBMI = Math.round( resultBMI * 10 ) / 10;
-        $('#result_bmi').text(`${resultBMI} BMI`);
+        if( parseFloat($('#input_cm').val()) > 0 ){
+            $('#result_bmi').text(`${resultBMI} BMI`);
+        }else{
+            $('#result_bmi').text(``);
+        }
     }else{
         $('#result_bmi').text(``);
     }
@@ -21,7 +29,11 @@ function calculateImperialBMI(){
         let meters = 0.3048 * parseFloat($('#input_ft').val()) + 0.0254 * parseFloat($('#input_in').val());
         resultBMI = kgs / Math.pow(meters, 2);
         resultBMI = Math.round( resultBMI * 10 ) / 10;
-        $('#result_bmi').text(`${resultBMI} BMI`);
+        if( meters > 0 ){
+            $('#result_bmi').text(`${resultBMI} BMI`);
+        }else{
+            $('#result_bmi').text(``);
+        }
     }else{
         $('#result_bmi').text(``);
     }
@@ -35,6 +47,113 @@ function calculateBMI() {
         calculateImperialBMI();
     }
 }
+
+// ###########################
+// ### Range and Number Inputs
+// ###########################
+
+// Imperial Weight Range
+$('#range_input_weight').on("input", function(){
+    $('#input_st').val( ( parseInt( $(this).val() ) - parseInt( $(this).val() ) % 14 ) / 14 );
+    $('#input_lbs').val( parseInt( $(this).val() ) % 14 );
+})
+$('#input_st').on("change", function(){
+    //max 25
+    if( parseInt( $(this).val() ) > 25 ){
+        $(this).val(25);
+    }
+    let lbs = parseInt( $('#input_lbs').val() );
+    if( parseInt( $(this).val() ) ){
+        if(lbs){
+            $('#range_input_weight').val( parseInt( $(this).val() ) * 14 + lbs );
+        }else{
+            $('#range_input_weight').val( parseInt( $(this).val() * 14 ) );
+        }        
+    }else if(lbs) {
+        $('#range_input_weight').val( lbs );
+    }else{
+       $('#range_input_weight').val( 0 ); 
+    }
+})
+$('#input_lbs').on("change", function(){
+    //max 13
+    if( parseInt( $(this).val() ) > 13 ){
+        $(this).val(13);
+    }
+    let st = parseInt( $('#input_st').val() );
+    if( parseInt( $(this).val() ) ){
+        if(st){
+            $('#range_input_weight').val( parseInt( $(this).val() ) + st * 14 );
+        }else{
+            $('#range_input_weight').val( parseInt( $(this).val() ) );
+        }        
+    }else if(st) {
+        $('#range_input_weight').val( st * 14 );
+    }else{
+       $('#range_input_weight').val( 0 ); 
+    }
+})
+
+// Imperial Height Range
+$('#range_input_height').on("input", function(){
+    $('#input_ft').val( ( parseInt( $(this).val() ) - parseInt( $(this).val() ) % 12 ) / 12 );
+    $('#input_in').val( parseInt( $(this).val() ) % 12 );
+})
+$('#input_ft').on("change", function(){
+    //max 8
+    if( parseInt( $(this).val() ) > 8 ){
+        $(this).val(8);
+    }
+    let lbs = parseInt( $('#input_in').val() );
+    if( parseInt( $(this).val() ) ){
+        if(lbs){
+            $('#range_input_height').val( parseInt( $(this).val() ) * 12 + lbs );
+        }else{
+            $('#range_input_height').val( parseInt( $(this).val() * 12 ) );
+        }        
+    }else if(lbs) {
+        $('#range_input_height').val( lbs );
+    }else{
+       $('#range_input_height').val( 0 ); 
+    }
+})
+$('#input_in').on("change", function(){
+    //max 11
+    if( parseInt( $(this).val() ) > 11 ){
+        $(this).val(11);
+    }
+    let st = parseInt( $('#input_ft').val() );
+    if( parseInt( $(this).val() ) ){
+        if(st){
+            $('#range_input_height').val( parseInt( $(this).val() ) + st * 12 );
+        }else{
+            $('#range_input_height').val( parseInt( $(this).val() ) );
+        }        
+    }else if(st) {
+        $('#range_input_height').val( st * 12 );
+    }else{
+       $('#range_input_height').val( 0 ); 
+    }
+})
+
+// Metric Weight Range
+
+
+
+
+
+
+
+
+
+
+// ########################################
+// ### DENY CHARACTERS IN NUBMER INPUTs ###
+// ########################################
+
+
+
+
 
 
 // Calculate whenever value is changed if all values are inputted
